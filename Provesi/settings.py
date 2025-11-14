ace2cdd7bb068e2a170d185695dd453f44145821
Diego Alejandro Munévar Pérez <d.munevar@uniandes.edu.co>
2025-11-13 19:44:42 -0500

    update

--- Provesi/settings.py
+++ Provesi/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-akq*=z3scq&s3h%39j6eyy$#60i-ux7d=t1uj(u-0sn3)npz2!
 DEBUG = True
 
 ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1"]
-
+CSRF_TRUSTED_ORIGINS = ["http://localhost", "http://127.0.0.1"]
+CSRF_COOKIE_SECURE = False
+CSRF_USE_SESSIONS = True
 
 # Application definition
 
